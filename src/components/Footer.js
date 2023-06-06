8b0b8b7f2eee35b2823443f8e0194312daeea2bb
Tim Dolan <timmydolan14@gmail.com>
2023-06-06 17:46:05 -0400

    Contact Form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,6 +12,9 @@ function Footer() {
         <ul>
         <a href="https://github.com/timdolan14">Github</a>
         </ul>
+        <ul>
+        <a href="https://twitter.com/timmydolan14">Twitter</a>
+        </ul>
       </p>
     </footer>
   );
