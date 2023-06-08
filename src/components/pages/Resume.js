2770dd7896c7190eaf563fbbdff07e02137f9854
Tim Dolan <timmydolan14@gmail.com>
2023-06-08 11:26:02 -0400

    Updated Resume Page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import resume from '../../photos/propic.png'
 
-export default function Resume () {
+export default function Resume() {
     return (
         <div className='res-page'>
             <h3>Want to Know More?</h3>
             <p>Feel to check out my resume below!</p>
-        <a href={resume} Download={resume} class="res-btn btn btn-primary btn-lg active" role="button" aria-pressed="true" id='res-btn'>Download My Resume</a>
+            <a href={resume} Download={resume} class="res-btn btn btn-primary btn-lg active" role="button" aria-pressed="true" id='res-btn'>Download My Resume</a>
+            <div className='profs'>
+                <h4>Front End</h4>
+                <p>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>React</li>
+                </p>
+                <h4>Back End</h4>
+                <p>
+                    <li>JavaScript</li>
+                    <li>SQL</li>
+                    <li>React</li>
+                    <li>MERN Stack</li>
+                    <li>GraphQL</li>              </p>
+            </div>
         </div>
     );
 }
