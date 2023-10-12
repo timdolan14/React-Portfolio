683dd74f1226ff8cdb349a0025b7b76d35e4bfc7
Tim Dolan <timmydolan14@gmail.com>
2023-10-12 11:14:12 -0400

    Updated Resume Page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import resume from '../../photos/Tim Dolan - Resume 2023.jpg'
+import resume from '../../photos/Timothy Dolan - Resume 2023.jpg'
 import { HiChevronRight } from "react-icons/hi";
 
 export default function Resume() {
